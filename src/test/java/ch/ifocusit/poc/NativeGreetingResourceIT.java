0ffe7c91e7cabcf4b67c129c5ab20c4f10a576a0
quarkusio <no-reply@quarkus.io>
2021-10-27 12:40:15 +0000

    Initial commit

---
+++ src/test/java/ch/ifocusit/poc/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package ch.ifocusit.poc;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
